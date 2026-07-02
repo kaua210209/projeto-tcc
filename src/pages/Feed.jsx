d12e3bfbd301b874b77303ee0ab85c4bc4205969
Kauã Lindão <0001083315@senaimgaluno.com.br>
2026-07-02 10:19:43 -0300

    icon localização corrigida

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,6 +4,19 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { useAuth } from '../contexts/AuthContext';
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 export default function Feed() {
   const { user } = useAuth();
